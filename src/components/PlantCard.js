b0107bd6a924c24942d752d0dde31ce6640094d7
Megan <esler6724@gmail.com>
2023-04-07 10:52:12 -0700

    commit tree 73e6ff667253956f6602d8536257c16f41486d74

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({
+  name,
+  image,
+  price
+}) {
+  const[inStock, setInStock] = useState(true)
+
+  function handleStock(inStock) {
+    setInStock(!inStock)
+  }
+
+  function handleBackInStock(inStock) {
+    setInStock(inStock)
+  }
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {inStock ? (
+        <button onClick={handleStock}className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleBackInStock}>Out of Stock</button>
       )}
     </li>
   );
